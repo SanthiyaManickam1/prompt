6a021ed21f75478e15a7597c155d39931975a5a9
SanthiyaManickam1 <santhiyamanickam2001@gmail.com>
2022-10-09 05:14:23 -0700

    commit message

---
+++ index.js
@@ -0,0 +1,26 @@
+/*let count=0;
+for(count=0;count<5;count++){
+    console.log(count);
+    */
+} 
+
+/*let a;      
+let array_name=[];
+for(count=0;count<5;count++)
+{
+    let a = prompt("Enter two numbers");
+    array_name = a;
+
+console.log(a);*/
+
+
+
+var total=0;
+
+for(let count=0;count<5;count++)
+{
+    let a = prompt("Enter the numbers");
+
+    total+=a;
+}
+console.log(total);
